e79f5c02cbbfb39a525e2daa3e22ad0959014030
damodara <jay108@mail.ru>
2025-07-24 23:36:15 +0300

    add sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def filter_by_state(data_list: list, state = "EXECUTED") -> list:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
 state соответствует указанному значению"""
@@ -6,3 +9,12 @@ state соответствует указанному значению"""
         if item.get("state") == state:
             filtered_list.append(item)
     return filtered_list
+
+
+def sort_by_date(data_list: list, reverse = True) -> list:
+    """Функция должна возвращать новый список, отсортированный по дате"""
+    return sorted(
+        data_list,
+        key=lambda x: datetime.fromisoformat(x['date']),
+        reverse=reverse
+    )
