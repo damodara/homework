7701447e5df33a20d448dd060f7267f9b5bd36ca
damodara <jay108@mail.ru>
2025-08-07 15:07:28 +0300

    upd test_masks.py & upd tests for get_mask_account & create conftest.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,33 +1,27 @@
 from src.masks import get_mask_account, get_mask_card_number
 
-
-def test_get_mask_card_number_correct_type() -> None:
-    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+from .conftest import invalid_account_numbers, invalid_card_numbers, valid_account_number, valid_card_number
 
 
-def test_get_mask_card_number_lenth() -> None:
-    assert get_mask_card_number("123456789012345") == "Номер карты должен быть строкой из 16 цифр"
-    assert get_mask_card_number("12345678901234567") == "Номер карты должен быть строкой из 16 цифр"
+def test_get_mask_card_number_correct_type(valid_card_number: str) -> None:
+    expected_result = "7000 79** **** 6361"
+    result = get_mask_card_number(valid_card_number)
+    assert result == expected_result
 
 
-def test_get_mask_card_number_isdigit() -> None:
-    assert get_mask_card_number("abcde1234567890") == "Номер карты должен быть строкой из 16 цифр"
-    assert get_mask_card_number("!@#$%^&*()_+") == "Номер карты должен быть строкой из 16 цифр"
+def test_get_mask_card_number_length(invalid_card_numbers: list) -> None:
+    for number in invalid_card_numbers:
+        result = get_mask_card_number(number)
+        assert result == "Номер карты должен быть строкой из 16 цифр"
 
 
-def test_get_mask_card_number_empty() -> None:
-    assert get_mask_card_number("") == "Номер карты должен быть строкой из 16 цифр"
+def test_get_mask_account_correct_type(valid_account_number: str) -> None:
+    expected_result = "**4305"
+    result = get_mask_account(valid_account_number)
+    assert result == expected_result
 
 
-def test_get_mask_account_correct_type() -> None:
-    assert get_mask_account("73654108430135874305") == "**4305"
-
-
-def test_get_mask_account_correct_lenth() -> None:
-    assert get_mask_account("1234567890123456789") == "Номер счета должен быть строкой из 20 цифр"
-    assert get_mask_account("123456789012345678901") == "Номер счета должен быть строкой из 20 цифр"
-
-
-def test_get_mask_account_correct_isdigit() -> None:
-    assert get_mask_account("abcde1234567890") == "Номер счета должен быть строкой из 20 цифр"
-    assert get_mask_account("!@#$%^&*()_+") == "Номер счета должен быть строкой из 20 цифр"
+def test_get_mask_account_length(invalid_account_numbers: list) -> None:
+    for number in invalid_account_numbers:
+        result = get_mask_account(number)
+        assert result == "Номер счета должен быть строкой из 20 цифр"
