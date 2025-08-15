217561bb4b3fc20207b5c9e2f13a84d08a6aeda0
damodara <jay108@mail.ru>
2025-08-15 15:12:42 +0300

    upd filter_by_currency in generators.py & main.py

--- src/generators.py
+++ src/generators.py
@@ -1,8 +1,18 @@
-def filter_by_currency(transactions: list, currency_code: str) -> dict:
-    """Функция должна возвращать итератор, который поочередно выдает транзакции, где валюта операции соответствует
-    заданной"""
+from typing import Any, Dict, Generator
+
+
+def filter_by_currency(
+    transactions: list[Dict[str, Any]], currency_code: str
+) -> Generator[Dict[str, Any], None, None]:
+    """
+    Возвращает итератор, выдающий транзакции, соответствующие указанной валюте.
+
+    :param transactions: Список словарей, каждый представляет отдельную транзакцию.
+    :param currency_code: Код валюты (например, "USD").
+    :return: Итератор, выдающий подходящие транзакции.
+    """
     return (
-        transaction for transaction in transactions
+        transaction
+        for transaction in transactions
         if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency_code
     )
-
