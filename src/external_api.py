f0e00d62f475779e65118271092aab40b4033c1e
damodara <jay108@mail.ru>
2025-08-29 16:08:47 +0300

    created utils.py with func load_transactions created external_api.py with func convert_currency

---
+++ src/external_api.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+API_TOKEN = os.getenv('API_KEY')
+HEADERS= {
+  "apikey": API_TOKEN
+}
+
+
+def convert_currency(amount: float, from_currency: str, to_currency: str = 'RUB') -> float:
+  """
+  Конвертирует указанную сумму из одной валюты в другую используя внешний API. Возвращает результат в указанной валюте.
+  :param amount: Сумма для конвертации
+  :param from_currency: Валюта исходной суммы (USD/EUR и др.)
+  :param to_currency: Валюта результата (по умолчанию RUB)
+  :return: Результат конвертации
+  """
+  BASE_URL = f"https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}"
+  # params = {'symbols': to_currency, 'base': from_currency}
+  # response = requests.get(BASE_URL, headers=HEADERS, params=params)
+  # response.raise_for_status()
+  #
+  # status_code = response.status_code
+  # result = response.text
+  # print(result)
+  response = requests.request("GET", BASE_URL, headers=HEADERS)
+  response.raise_for_status()
+  data = response.json()
+  return data['result']
+
